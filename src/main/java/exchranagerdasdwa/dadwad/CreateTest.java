f203d285a79c0107e6dc4a4981ef477035b10439
晓黑 <3270957121@qq.com>
2020-12-06 01:53:54 +0800

    jhhgdfsa

--- src/main/java/exchranagerdasdwa/dadwad/CreateTest.java
+++ src/main/java/exchranagerdasdwa/dadwad/CreateTest.java
@@ -1,14 +1,1 @@
-package exchranagerdasdwa.dadwad;
-
-/**
- * @Description TODO
- * @Author liangze
- * @Date 2020/12/6 0:53
- * @Version 1.0
- */
-public class CreateTest {
-    public static void main(String[] args) {
-
-        System.out.println("今天心情真好！！n dasyfdytasgdtysagdaaftd");
-    }
-}
+package exc
