857c97070aff50d76fd1feb954eef4e7bc14bcbd
晓黑 <3270957121@qq.com>
2020-12-06 01:10:46 +0800

    转正添加

--- src/main/java/exchranagerdasdwa/dadwad/CreateTest.java
+++ src/main/java/exchranagerdasdwa/dadwad/CreateTest.java
@@ -9,6 +9,6 @@ package exchranagerdasdwa.dadwad;
 public class CreateTest {
     public static void main(String[] args) {
 
-        System.out.println("今天心情真好！！转正");
+        System.out.println("今天心情真好！！转正 ");
     }
 }
